8bc7628d8689cf94386cd93e19d4fce325bd3b16
HarryMinasyan <>
2021-12-28 23:48:03 -0800

    c

---
+++ serverConfig/scripts/restart_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# restart the nginx server
+sudo service restart nginx
+# log the status 
+# service nginx status
+
+# node restartsudo 
+# cd /var/www/socal/api
+sudo pm2 restart socal
